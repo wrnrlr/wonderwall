6b9d6430f85fc5252996a497619a66fcc3a09a3a
Werner Laurensse <werner@alman.ax>
2020-09-13 02:19:59 +0200

    Move wall service to wall file

--- server.go
+++ server.go
@@ -144,38 +144,6 @@ func (s Sessions) CreateSession(*Txn, string) (*Session, error) {
 	return nil, nil
 }
 
-type Wall struct {
-	ID      string
-	Content []interface{}
-}
-
-func (w *Wall) Key() Key {
-	if w == nil {
-		return Key("wall:")
-	} else {
-		return Key("wall:" + w.ID)
-	}
-}
-
-type CreateWall interface {
-	CreateWall(*Txn, *User) (*Wall, error)
-}
-type FindWallById interface {
-	FindWallById(*Txn, string) (*Wall, error)
-}
-type UpdateWall interface {
-	UpdateWall(*Txn, *Wall) error
-}
-type DeleteWall interface {
-	DeleteWall(*Txn, *Wall) error
-}
-type Walls struct{}
-
-func (w Walls) CreateWall(*Txn, *User) (*Wall, error)    { return nil, nil }
-func (w Walls) FindWallById(*Txn, string) (*Wall, error) { return nil, nil }
-func (w Walls) UpdateWall(*Txn, *Wall) error             { return nil }
-func (w Walls) DeleteWall(*Txn, *Wall) error             { return nil }
-
 type FindUserByEmail interface {
 	FindUserByEmail(*Txn, Email) (*User, error)
 }
