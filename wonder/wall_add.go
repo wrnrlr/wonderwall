143388bbee723863e78df937ebdbd7cff2830c44
Werner Laurensse <werner@alman.ax>
2020-09-26 19:47:57 +0200

    New wall form

--- wonder/wall_add.go
+++ wonder/wall_add.go
@@ -3,17 +3,41 @@ package main
 import (
 	"gioui.org/layout"
 	"gioui.org/unit"
+	"gioui.org/widget"
 	"github.com/Almanax/wonderwall/wonder/ui"
 )
 
 type AddWallPage struct {
 	env    *Env
-	topbar *Topbar
 	list   *layout.List
+	topbar *Topbar
+	fields []*Field
+	submit *widget.Clickable
+}
+
+type wallForm struct {
+	Workspace string
+	Title     string
 }
 
 func NewAddWallPage(env *Env) *AddWallPage {
-	return &AddWallPage{env: env, topbar: &Topbar{Back: true}}
+	form := &wallForm{}
+	p := &AddWallPage{
+		env:    env,
+		list:   &layout.List{Axis: layout.Vertical},
+		topbar: &Topbar{Back: true},
+		fields: []*Field{
+			{Header: "Workspace", Hint: "", Value: &form.Workspace},
+			{Header: "Title", Hint: "", Value: &form.Title}},
+		submit: &widget.Clickable{}}
+	for _, f := range p.fields {
+		f.env = p.env
+		f.edit = &widget.Editor{
+			SingleLine: true,
+		}
+		f.edit.SetText(*f.Value)
+	}
+	return p
 }
 
 func (p *AddWallPage) Start(stop <-chan struct{}) {}
@@ -46,5 +70,25 @@ func (p *AddWallPage) LayoutMenu(gtx C) D {
 }
 
 func (p *AddWallPage) LayoutContent(gtx C) D {
-	return D{}
+	l := p.list
+	inset := layout.Inset{
+		Left:  unit.Max(gtx.Metric, unit.Dp(32), p.env.insets.Left),
+		Right: unit.Max(gtx.Metric, unit.Dp(32), p.env.insets.Right),
+	}
+	return l.Layout(gtx, len(p.fields)+1, func(gtx C, i int) D {
+		in := inset
+		switch {
+		case i < len(p.fields):
+			in.Bottom = unit.Dp(12)
+			if i == 0 {
+				in.Top = unit.Dp(32)
+			}
+			return in.Layout(gtx, p.fields[i].Layout)
+		default:
+			in.Bottom = unit.Max(gtx.Metric, unit.Dp(32), p.env.insets.Bottom)
+			return layout.W.Layout(gtx, func(gtx C) D {
+				return in.Layout(gtx, ui.Button(theme, p.submit, "Create").Layout)
+			})
+		}
+	})
 }
