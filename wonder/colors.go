6f55508da2fa0381be4b074531e8e52f53d443bc
Werner Laurensse <werner@alman.ax>
2020-09-29 02:58:07 +0200

    Colors

---
+++ wonder/colors.go
@@ -0,0 +1,12 @@
+package main
+
+import "image/color"
+
+var (
+	maroon    = color.RGBA{127, 0, 0, 255}
+	lightgrey = color.RGBA{100, 100, 100, 255}
+	black     = color.RGBA{0, 0, 0, 255}
+	red       = color.RGBA{255, 0, 0, 255}
+	green     = color.RGBA{0, 255, 0, 255}
+	blue      = color.RGBA{0, 0, 255, 255}
+)
