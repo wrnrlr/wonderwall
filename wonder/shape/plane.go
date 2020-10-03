3ea3e91a99cd62163f4db1fa5eee71d1c2026252
Werner Laurensse <werner@alman.ax>
2020-10-03 06:58:59 +0200

    Plane: rename Add to Insert

--- wonder/shape/plane.go
+++ wonder/shape/plane.go
@@ -2,11 +2,13 @@ package shape
 
 import (
 	"gioui.org/f32"
+	"github.com/Almanax/wonderwall/wonder/rtree"
 )
 
 // A two-dimensional surface that extends infinitely far
 type Plane struct {
 	Elements Group
+	Index    rtree.RTree
 }
 
 func (p *Plane) View(r f32.Rectangle, gtx C) {
@@ -19,12 +21,34 @@ func (p *Plane) View(r f32.Rectangle, gtx C) {
 	//}
 }
 
-func (p *Plane) Add(s Shape) {
+func (p *Plane) Insert(s Shape) {
 	p.Elements.Append(s)
+	bounds := s.Bounds()
+	min, max := [2]float32{bounds.Min.X, bounds.Min.Y}, [2]float32{bounds.Max.X, bounds.Max.Y}
+	p.Index.Insert(min, max, s)
 }
 
 func (p Plane) Within(r f32.Rectangle) Group {
+	min, max := [2]float32{r.Min.X, r.Min.Y}, [2]float32{r.Max.X, r.Max.Y}
+	p.Index.Search(min, max, func(min [2]float32, max [2]float32, value interface{}) bool {
+
+		return false
+	})
 	return Group{}
+}
+
+func (p Plane) Intersects(r f32.Rectangle) []Shape {
+	var results []Shape
+	min, max := [2]float32{r.Min.X, r.Min.Y}, [2]float32{r.Max.X, r.Max.Y}
+	p.Index.Search(min, max, func(min [2]float32, max [2]float32, value interface{}) bool {
+		s, ok := value.(Shape)
+		if !ok {
+			return false
+		}
+		results = append(results, s)
+		return true
+	})
+	return results
 }
 
 func intersects(r1, r2 f32.Rectangle) bool {
