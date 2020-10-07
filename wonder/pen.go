6513922ea1fb25c67e3c056994e7cea15c89ce85
Werner Laurensse <werner@alman.ax>
2020-10-07 09:01:57 +0200

    Scale pen event positions based on 1 over PxPerDp

--- wonder/pen.go
+++ wonder/pen.go
@@ -4,7 +4,7 @@ import (
 	"gioui.org/f32"
 	"gioui.org/io/pointer"
 	"gioui.org/op"
-	"github.com/wrnrlr/polyline"
+	"github.com/Almanax/wonderwall/wonder/shape"
 	"image"
 	"image/color"
 )
@@ -16,12 +16,13 @@ type Pen struct {
 
 func (p *Pen) Draw(gtx C, width float32, col color.RGBA) {
 	if p.events != nil {
-		polyline.Draw(p.events, width, col, gtx)
+		shape.Polyline{Points: p.events, Width: width, Color: col}.Draw(gtx)
 	}
 }
 
 func (p *Pen) Event(gtx C) []f32.Point {
 	var l []f32.Point
+	scale := 1 / gtx.Metric.PxPerDp
 	defer op.Push(gtx.Ops).Pop()
 	pointer.Rect(image.Rectangle{Max: gtx.Constraints.Min}).Add(gtx.Ops)
 	for _, e := range gtx.Events(p) {
@@ -29,13 +30,14 @@ func (p *Pen) Event(gtx C) []f32.Point {
 		if !ok {
 			continue
 		}
+		pos := e.Position.Mul(scale)
 		switch e.Type {
 		case pointer.Press:
-			p.events = append(p.events, e.Position)
+			p.events = append(p.events, pos)
 		case pointer.Drag:
-			p.events = append(p.events, e.Position)
+			p.events = append(p.events, pos)
 		case pointer.Release, pointer.Cancel:
-			l = append(p.events, e.Position)
+			l = append(p.events, pos)
 			p.events = nil
 		}
 	}
