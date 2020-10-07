4392e64e4657f03b9f38bd14a1080d9885c8efdd
Werner Laurensse <werner@alman.ax>
2020-10-07 12:38:33 +0200

    Add constructors function, and RemoveAll method for Plane

--- wonder/shape/plane.go
+++ wonder/shape/plane.go
@@ -7,14 +7,22 @@ import (
 
 // A two-dimensional surface that extends infinitely far
 type Plane struct {
-	Elements Group
+	Elements map[string]Shape
 	Index    rtree.RTree
+}
+
+func NewPlane() *Plane {
+	return &Plane{
+		Elements: map[string]Shape{},
+	}
 }
 
 func (p *Plane) View(r f32.Rectangle, gtx C) {
 	// Find elements within r
 	//offset := f32.Pt(r.Dx(), r.Dy())
-	p.Elements.Draw(gtx)
+	for _, s := range p.Elements {
+		s.Draw(gtx)
+	}
 	//for _, s := range p.Elements {
 	//if intersects(r, s.Bounds()) {
 	//	s.Draw(gtx)
@@ -22,7 +30,7 @@ func (p *Plane) View(r f32.Rectangle, gtx C) {
 }
 
 func (p *Plane) Insert(s Shape) {
-	p.Elements.Append(s)
+	p.Elements[s.Identity()] = s
 	bounds := s.Bounds()
 	min, max := [2]float32{bounds.Min.X, bounds.Min.Y}, [2]float32{bounds.Max.X, bounds.Max.Y}
 	p.Index.Insert(min, max, s)
@@ -65,6 +73,15 @@ func (p Plane) Hits(pos f32.Point) Shape {
 		return false
 	})
 	return result
+}
+
+func (p *Plane) RemoveAll(ss []Shape) {
+	for _, s := range ss {
+		delete(p.Elements, s.Identity())
+		bounds := s.Bounds()
+		min, max := [2]float32{bounds.Min.X, bounds.Min.Y}, [2]float32{bounds.Max.X, bounds.Max.Y}
+		p.Index.Delete(min, max, s)
+	}
 }
 
 func intersects(r1, r2 f32.Rectangle) bool {
