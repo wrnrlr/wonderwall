2c30c58ed882a17ae584393de5a2f0fe2f648b5d
Werner Laurensse <werner@alman.ax>
2020-10-15 13:28:33 +0200

    Add action file

---
+++ wonder/action.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/Almanax/wonderwall/wonder/shape"
+
+type Diff struct {
+	Prev []shape.Shape
+	Next []shape.Shape
+}
+
+func (a Diff) Apply(plane shape.Plane) {
+	if a.Prev == nil {
+		plane.InsertAll(a.Next)
+	} else if a.Next == nil {
+		plane.RemoveAll(a.Next)
+	} else {
+		plane.UpdateAll(a.Next)
+	}
+}
+
+func (a Diff) Invert() Diff {
+	return Diff{Prev: a.Next, Next: a.Prev}
+}
+
+type State struct {
+}
