d51286a52c0a342e2a11cdee35c37e9816bcb41c
Werner Laurensse <werner@alman.ax>
2021-01-14 09:40:42 +0100

    Reimplement Polyline.Draw() using gio builtin stroke api

--- wonder/shape/line.go
+++ wonder/shape/line.go
@@ -2,7 +2,6 @@ package shape
 
 import (
 	"gioui.org/f32"
-	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
@@ -58,7 +57,6 @@ func (l *Polyline) Bounds() f32.Rectangle {
 				l.rects = append(l.rects, rect{a, b, c, d})
 				box := boundingBox([]f32.Point{a, b, c, d})
 				l.boxes = append(l.boxes, box)
-				//pointer.InputOp{Tag: &l, Grab: false, Types: pointer.Press | pointer.Drag | pointer.Release}.Insert(gtx.Ops)
 			}
 		}
 	}
@@ -79,10 +77,23 @@ func (l *Polyline) Offset(p f32.Point) Shape {
 	return l
 }
 
+var red = color.NRGBA{R: 255, A: 255}
+
 func (l Polyline) Draw(gtx C) {
 	scale := gtx.Metric.PxPerDp
 	width := l.Width * scale
-	l.drawPolyline(l.Points, width, l.Color, gtx)
+	defer op.Save(gtx.Ops).Load()
+	var path clip.Path
+	path.Begin(gtx.Ops)
+	for i, p := range l.Points {
+		if i == 0 {
+			path.MoveTo(p.Mul(scale))
+		} else {
+			path.LineTo(p.Mul(scale))
+		}
+	}
+	style := clip.StrokeStyle{Width: width, Miter: 10, Cap: clip.RoundCap, Join: clip.RoundJoin}
+	paint.FillShape(gtx.Ops, red, clip.Stroke{Path: path.End(), Style: style}.Op())
 }
 
 func (l *Polyline) Move(delta f32.Point) {
@@ -94,52 +105,7 @@ func (l *Polyline) Move(delta f32.Point) {
 }
 
 func (l Polyline) drawPolyline(points []f32.Point, width float32, col color.NRGBA, gtx C) {
-	scale := gtx.Metric.PxPerDp
-	length := len(points)
-	for i, p := range points {
-		p = p.Mul(scale)
-		l.drawCircle(p, width, col, gtx)
-		if i < length-1 {
-			p2 := points[i+1].Mul(scale)
-			l.drawLine(p, p2, width, col, gtx)
-		}
-	}
-}
 
-func (l Polyline) drawCircle(p f32.Point, radius float32, col color.NRGBA, gtx C) {
-	const k = 0.551915024494 // 4*(sqrt(2)-1)/3
-	ops := gtx.Ops
-	defer op.Save(gtx.Ops).Load()
-	var path clip.Path
-	path.Begin(ops)
-	path.Move(f32.Point{X: p.X + radius, Y: p.Y})
-	path.Cube(f32.Point{X: 0, Y: radius * k}, f32.Point{X: -radius + radius*k, Y: radius}, f32.Point{X: -radius, Y: radius})    // SE
-	path.Cube(f32.Point{X: -radius * k, Y: 0}, f32.Point{X: -radius, Y: -radius + radius*k}, f32.Point{X: -radius, Y: -radius}) // SW
-	path.Cube(f32.Point{X: 0, Y: -radius * k}, f32.Point{X: radius - radius*k, Y: -radius}, f32.Point{X: radius, Y: -radius})   // NW
-	path.Cube(f32.Point{X: radius * k, Y: 0}, f32.Point{X: radius, Y: radius - radius*k}, f32.Point{X: radius, Y: radius})      // NE
-	clip.Outline{Path: path.End()}.Op().Add(gtx.Ops)
-	paint.ColorOp{Color: col}.Add(ops)
-	paint.PaintOp{}.Add(ops)
-}
-
-func (l Polyline) drawLine(p1, p2 f32.Point, width float32, col color.NRGBA, gtx layout.Context) {
-	tilt := angle(p1, p2)
-	a := offsetPoint(p1, width, tilt+rad90)
-	b := offsetPoint(p2, width, tilt+rad90)
-	c := offsetPoint(p2, -width, tilt+rad90)
-	d := offsetPoint(p1, -width, tilt+rad90)
-	stack := op.Save(gtx.Ops)
-	paint.ColorOp{Color: col}.Add(gtx.Ops)
-	var path clip.Path
-	path.Begin(gtx.Ops)
-	path.Move(a)
-	path.Line(b.Sub(a))
-	path.Line(c.Sub(b))
-	path.Line(d.Sub(c))
-	path.Line(a.Sub(d))
-	clip.Outline{Path: path.End()}.Op().Add(gtx.Ops)
-	paint.PaintOp{}.Add(gtx.Ops)
-	stack.Load()
 }
 
 func (l *Polyline) Hit(p f32.Point) bool {
