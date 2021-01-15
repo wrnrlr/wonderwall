9e2e4cdbb00dbfa6556e850150ce1605ace098c6
Werner Laurensse <werner@alman.ax>
2021-01-15 11:49:03 +0100

    Add colorpicker example

---
+++ wonder/ui/example/color.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/unit"
+	"github.com/Almanax/wonderwall/wonder/ui"
+	"image/color"
+)
+
+var red = color.NRGBA{R: 255, A: 255}
+
+func main() {
+	go func() {
+		w := app.NewWindow(app.Size(unit.Dp(800), unit.Dp(700)))
+		th := ui.MenuTheme(gofont.Collection())
+		colorpicker := ui.Color(th, red)
+		var ops op.Ops
+		for {
+			e := <-w.Events()
+			switch e := e.(type) {
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+				colorpicker.Event(gtx)
+				colorpicker.Layout(gtx)
+				e.Frame(gtx.Ops)
+			}
+		}
+	}()
+	app.Main()
+}
