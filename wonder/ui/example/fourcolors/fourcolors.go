6cc7cd1e2457236232dcf325d7ccef7db8d776bb
Werner Laurensse <werner@alman.ax>
2021-02-01 02:59:33 +0100

    Draw rainbow based on rgb values of red, yellow, green, cyan, blue and magenta

--- wonder/ui/example/fourcolors/fourcolors.go
+++ wonder/ui/example/fourcolors/fourcolors.go
@@ -49,6 +49,7 @@ var primary = f32color.RGBAToNRGBA(color.RGBA{G: 0xff, A: 0xff})
 
 func (cp *ColorPicker) Layout(gtx layout.Context) layout.Dimensions {
 	return layout.UniformInset(unit.Dp(5)).Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+		gtx.Constraints = layout.Exact(image.Point{X: gtx.Px(unit.Dp(210)), Y: gtx.Px(unit.Dp(200))})
 		return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 			layout.Rigid(cp.layoutGradiants),
 			layout.Rigid(cp.layoutRainbow),
@@ -57,7 +58,7 @@ func (cp *ColorPicker) Layout(gtx layout.Context) layout.Dimensions {
 }
 
 func (cp *ColorPicker) layoutGradiants(gtx layout.Context) layout.Dimensions {
-	w := gtx.Px(unit.Dp(200))
+	w := gtx.Constraints.Max.X
 	h := gtx.Px(unit.Dp(120))
 	dr := image.Rectangle{Max: image.Point{X: w, Y: h}}
 	stack := op.Save(gtx.Ops)
@@ -90,30 +91,28 @@ func (cp *ColorPicker) layoutGradiants(gtx layout.Context) layout.Dimensions {
 }
 
 var (
-	red     = color.NRGBA{R: 255, A: 255}
-	yellow  = color.NRGBA{R: 255, G: 255, A: 255}
-	green   = color.NRGBA{G: 255, A: 255}
-	cyan    = color.NRGBA{G: 255, B: 255, A: 255}
-	skyblue = color.NRGBA{G: 127, B: 255, A: 255}
-	blue    = color.NRGBA{B: 255, A: 255}
-	purple  = color.NRGBA{R: 127, G: 0, B: 255, A: 255}
-	magenta = color.NRGBA{R: 255, B: 255, A: 255}
+	red     = color.RGBA{R: 255, A: 255}
+	yellow  = color.RGBA{R: 255, G: 255, A: 255}
+	green   = color.RGBA{G: 255, A: 255}
+	cyan    = color.RGBA{G: 255, B: 255, A: 255}
+	skyblue = color.RGBA{G: 127, B: 255, A: 255}
+	blue    = color.RGBA{B: 255, A: 255}
+	purple  = color.RGBA{R: 127, G: 0, B: 255, A: 255}
+	magenta = color.RGBA{R: 255, B: 255, A: 255}
 )
 
-var colors = []color.NRGBA{red, yellow, green, cyan, skyblue, blue, purple, magenta, red}
+var colors = []color.RGBA{red, yellow, green, cyan, blue, magenta, red}
 
 func (cp *ColorPicker) layoutRainbow(gtx layout.Context) layout.Dimensions {
-	w := gtx.Px(unit.Dp(200))
+	w := gtx.Constraints.Max.X
 	h := gtx.Px(unit.Dp(20))
-	stepCount := 9
+	stepCount := len(colors)
 	stepWidth := float32(w / (stepCount - 1))
-	step := uint8((256 / stepCount) - 1)
-	value := uint8(0)
 	offsetX := float32(0)
-	color1 := f32color.RGBAToNRGBA(HsvToRgb(HSVColor{H: 0, S: 1, V: 1}))
-	for i := 1; i < stepCount; i++ {
-		//nr := uint(i)
-		color2 := f32color.RGBAToNRGBA(HsvToRgb(HSVColor{H: float32(i * 60), S: 1, V: 1}))
+	color1 := f32color.RGBAToNRGBA(colors[0])
+	dr := image.Rectangle{Max: image.Point{X: int(stepWidth), Y: h}}
+	for _, col := range colors[1:] {
+		color2 := f32color.RGBAToNRGBA(col)
 		stack := op.Save(gtx.Ops)
 		paint.LinearGradientOp{
 			Stop1:  f32.Point{offsetX, 0},
@@ -121,19 +120,18 @@ func (cp *ColorPicker) layoutRainbow(gtx layout.Context) layout.Dimensions {
 			Color1: color1,
 			Color2: color2,
 		}.Add(gtx.Ops)
-		dr := image.Rectangle{Min: image.Point{X: int(offsetX), Y: 0}, Max: image.Point{X: int(offsetX + stepWidth), Y: h}}
 		clip.Rect(dr).Add(gtx.Ops)
 		paint.PaintOp{}.Add(gtx.Ops)
 		stack.Load()
-		offsetX += stepWidth
-		value += step
 		color1 = color2
+		offsetX += stepWidth
+		dr = image.Rectangle{Min: image.Point{X: int(offsetX), Y: 0}, Max: image.Point{X: int(offsetX + stepWidth), Y: h}}
 	}
 	return layout.Dimensions{Size: image.Point{X: w, Y: h}}
 }
 
 func (cp *ColorPicker) layoutAlpha(gtx layout.Context) layout.Dimensions {
-	w := gtx.Px(unit.Dp(200))
+	w := gtx.Constraints.Max.X
 	h := gtx.Px(unit.Dp(20))
 	defer op.Save(gtx.Ops).Load()
 	paint.LinearGradientOp{
