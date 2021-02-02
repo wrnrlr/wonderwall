95bacf02614b46d3704979cff09fe4646b5a5c92
Werner Laurensse <werner@alman.ax>
2021-02-02 18:46:10 +0100

    Fix HsvToRgb test

--- wonder/ui/example/fourcolors/hsv_test.go
+++ wonder/ui/example/fourcolors/hsv_test.go
@@ -19,28 +19,28 @@ func TestRgbHsvConversion(t *testing.T) {
 		{color.RGBA{R: 0xff, G: 0xff, A: 0xff}, HSVColor{H: 60, S: 1, V: 1}},           // yellow
 		{color.RGBA{G: 0xff, B: 0xff, A: 0xff}, HSVColor{H: 180, S: 1, V: 1}},          // cyan
 		{color.RGBA{R: 0xff, B: 0xff, A: 0xff}, HSVColor{H: 300, S: 1, V: 1}},          // magenta
-		{color.RGBA{R: 0xbf, G: 0xbf, B: 0xbf, A: 0xff}, HSVColor{H: 0, S: 1, V: .75}}, // silver
-		{color.RGBA{R: 0x80, G: 0x80, B: 0x80, A: 0xff}, HSVColor{H: 0, S: 1, V: .5}},  // gray
-		{color.RGBA{R: 0x80, A: 0xff}, HSVColor{H: 0, S: 1, V: .5}},                    // maroon
-		{color.RGBA{R: 0xff, G: 0x80, A: 0xff}, HSVColor{H: 60, S: 1, V: .5}},          // olive
-		{color.RGBA{G: 0x80, A: 0xff}, HSVColor{H: 120, S: 1, V: .5}},                  // green
-		{color.RGBA{R: 0x80, B: 0x80, A: 0xff}, HSVColor{H: 300, S: 1, V: .5}},         // purple
-		{color.RGBA{G: 0x80, B: 0x80, A: 0xff}, HSVColor{H: 180, S: 1, V: .5}},         // teal
-		{color.RGBA{B: 0x80, A: 0xff}, HSVColor{H: 240, S: 1, V: .5}},                  // navy
+		{color.RGBA{R: 0xbf, G: 0xbf, B: 0xbf, A: 0xff}, HSVColor{H: 0, S: 0, V: .75}}, // silver
+		{color.RGBA{R: 0x7f, G: 0x7f, B: 0x7f, A: 0xff}, HSVColor{H: 0, S: 0, V: .5}},  // gray
+		{color.RGBA{R: 0x7f, A: 0xff}, HSVColor{H: 0, S: 1, V: .5}},                    // maroon
+		{color.RGBA{R: 0xff, G: 0x7f, A: 0xff}, HSVColor{H: 60, S: 1, V: .5}},          // olive
+		{color.RGBA{G: 0x7f, A: 0xff}, HSVColor{H: 120, S: 1, V: .5}},                  // green
+		{color.RGBA{R: 0x7f, B: 0x7f, A: 0xff}, HSVColor{H: 300, S: 1, V: .5}},         // purple
+		{color.RGBA{G: 0x7f, B: 0x7f, A: 0xff}, HSVColor{H: 180, S: 1, V: .5}},         // teal
+		{color.RGBA{B: 0x7f, A: 0xff}, HSVColor{H: 240, S: 1, V: .5}},                  // navy
 	}
 	for _, v := range rgbHsvs {
-		testHsv(v.hsv, RgbToHsv(v.rgb))
-		testRgb(v.rgb, HsvToRgb(v.hsv))
+		testHsvToRgb(v.hsv, RgbToHsv(v.rgb))
+		testRgbToHsv(v.rgb, HsvToRgb(v.hsv))
 	}
 }
 
-func testRgb(a color.RGBA, b color.RGBA) {
+func testRgbToHsv(a color.RGBA, b color.RGBA) {
 	if a.R != b.R && a.G != b.G && a.B != b.B {
 		panic(fmt.Sprintf("not equal: %v, %v", a, b))
 	}
 }
 
-func testHsv(a HSVColor, b HSVColor) {
+func testHsvToRgb(a HSVColor, b HSVColor) {
 	if a.H != b.H && a.S != b.S && a.V != b.V {
 		panic(fmt.Sprintf("not equal: %v, %v", a, b))
 	}
