2c4992817d70fc969c58ba9d72cca9693c8bf4b1
Werner Laurensse <werner@alman.ax>
2021-02-22 05:49:31 +0100

    colorpicker: Use fields for editor

--- colorpicker/editor.go
+++ colorpicker/editor.go
@@ -2,28 +2,23 @@ package colorpicker
 
 import (
 	"encoding/hex"
-	"fmt"
 	"gioui.org/layout"
 	"gioui.org/unit"
-	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"image/color"
 	"strconv"
-	"strings"
 )
-
-type EditorType int
 
 type HexEditor struct {
 	theme *material.Theme
 	color color.NRGBA
-	hex   *widget.Editor
+	hex   *hexField
 }
 
 func (e *HexEditor) Layout(gtx layout.Context) layout.Dimensions {
 	return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceAround}.Layout(gtx,
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "Hex").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.hex, "").Layout))
+		layout.Flexed(1, material.Editor(e.theme, &e.hex.Editor, "").Layout))
 }
 
 func (e *HexEditor) Color() color.NRGBA {
@@ -37,48 +32,29 @@ func (e *HexEditor) SetColor(col color.NRGBA) {
 }
 
 func (e *HexEditor) Changed() bool {
-	if b, ok := hexFromEditor(e.hex); ok {
-		e.color.R = b[0]
-		e.color.G = b[1]
-		e.color.B = b[2]
-		return true
-	}
-	return false
+	return e.hex.Changed()
 }
 
 type RgbEditor struct {
 	theme *material.Theme
 	rgb   color.NRGBA
-	r     *widget.Editor
-	g     *widget.Editor
-	b     *widget.Editor
+	r     *byteField
+	g     *byteField
+	b     *byteField
 }
 
 func (e *RgbEditor) Layout(gtx layout.Context) layout.Dimensions {
 	return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceAround, Alignment: layout.Baseline}.Layout(gtx,
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "R ").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.r, "").Layout),
+		layout.Flexed(1, material.Editor(e.theme, &e.r.Editor, "").Layout),
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "G ").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.g, "").Layout),
+		layout.Flexed(1, material.Editor(e.theme, &e.g.Editor, "").Layout),
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "B ").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.b, "").Layout))
+		layout.Flexed(1, material.Editor(e.theme, &e.b.Editor, "").Layout))
 }
 
 func (e *RgbEditor) Changed() bool {
-	changed := false
-	if b, ok := byteFromEditor(e.r); ok {
-		e.rgb.R = b
-		changed = true
-	}
-	if b, ok := byteFromEditor(e.g); ok {
-		e.rgb.R = b
-		changed = true
-	}
-	if b, ok := byteFromEditor(e.b); ok {
-		e.rgb.B = b
-		changed = true
-	}
-	return changed
+	return e.r.Changed() || e.g.Changed() || e.b.Changed()
 }
 
 func (e *RgbEditor) SetColor(col color.NRGBA) {
@@ -92,22 +68,26 @@ func (e *RgbEditor) SetColor(col color.NRGBA) {
 	e.Changed()
 }
 
+func (e *RgbEditor) Color() color.NRGBA {
+	return e.rgb
+}
+
 type HsvEditor struct {
 	theme *material.Theme
 	hsv   HSVColor
-	h     *widget.Editor
-	s     *widget.Editor
-	v     *widget.Editor
+	h     *degreeField
+	s     *percentageField
+	v     *percentageField
 }
 
 func (e *HsvEditor) Layout(gtx layout.Context) layout.Dimensions {
 	return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceAround, Alignment: layout.Baseline}.Layout(gtx,
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "H ").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.h, "").Layout),
+		layout.Flexed(1, material.Editor(e.theme, &e.h.Editor, "").Layout),
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "S ").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.s, "").Layout),
+		layout.Flexed(1, material.Editor(e.theme, &e.s.Editor, "").Layout),
 		layout.Rigid(material.Label(e.theme, unit.Sp(14), "V ").Layout),
-		layout.Flexed(1, material.Editor(e.theme, e.v, "").Layout))
+		layout.Flexed(1, material.Editor(e.theme, &e.v.Editor, "").Layout))
 }
 
 func (e *HsvEditor) Color() color.NRGBA {
@@ -115,20 +95,7 @@ func (e *HsvEditor) Color() color.NRGBA {
 }
 
 func (e *HsvEditor) Changed() bool {
-	changed := false
-	if h, ok := degreeFromEditor(e.h); ok {
-		e.hsv.H = float32(h)
-		changed = true
-	}
-	if b, ok := percentageFromEditor(e.s); ok {
-		e.hsv.S = float32(b / 100)
-		changed = true
-	}
-	if b, ok := percentageFromEditor(e.v); ok {
-		e.hsv.V = float32(b / 100)
-		changed = true
-	}
-	return changed
+	return e.h.Changed() || e.s.Changed() || e.v.Changed()
 }
 
 func (e *HsvEditor) SetColor(col color.NRGBA) {
@@ -142,87 +109,49 @@ func (e *HsvEditor) SetColor(col color.NRGBA) {
 	e.Changed()
 }
 
-func byteFromEditor(e *widget.Editor) (byte, bool) {
-	b := byte(0)
-	changed := false
-	for _, ev := range e.Events() {
-		_, ok := ev.(widget.ChangeEvent)
-		if !ok {
-			continue
-		}
-		s := e.Text()
-		i, err := strconv.ParseInt(s, 10, 8)
-		if err != nil {
-			continue
-		}
-		b, changed = byte(i), true
+func parseHex(s string) ([]byte, bool) {
+	out, err := hex.DecodeString(s)
+	if err != nil {
+		return nil, false
 	}
-	return b, changed
+	if len(out) < 3 {
+		return nil, false
+	}
+	return out, true
 }
 
-func percentageFromEditor(e *widget.Editor) (int, bool) {
-	percentage := 0
-	changed := false
-	for _, ev := range e.Events() {
-		_, ok := ev.(widget.ChangeEvent)
-		if !ok {
-			continue
-		}
-		s := e.Text()
-		i, err := strconv.ParseInt(s, 10, 32)
-		if err != nil {
-			continue
-		}
-		b := int(i)
-		percentage, changed = b, true
+func parseByte(s string) (byte, bool) {
+	i, err := strconv.ParseUint(s, 10, 8)
+	if err != nil {
+		return 0, false
 	}
-	return percentage, changed
+	return byte(i), true
 }
 
-func degreeFromEditor(e *widget.Editor) (int, bool) {
-	degree := 0
-	changed := false
-	for _, ev := range e.Events() {
-		_, ok := ev.(widget.ChangeEvent)
-		if !ok {
-			continue
-		}
-		s := e.Text()
-		i, err := strconv.ParseInt(s, 10, 32)
-		if err != nil {
-			continue
-		}
-		degree, changed = int(i), true
+func parseDegree(s string) (int, bool) {
+	i, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		return int(i), false
 	}
-	return degree, changed
+	return 0, true
 }
 
-func hexFromEditor(e *widget.Editor) ([]byte, bool) {
-	h, changed := []byte(nil), false
-	for _, ev := range e.Events() {
-		ce, ok := ev.(widget.ChangeEvent)
-		if !ok {
-			continue
-		}
-		s := e.Text()
-		out, err := hex.DecodeString(s)
-		if err != nil {
-			continue
-		}
-		if len(out) < 3 {
-			continue
-		}
-		fmt.Printf("%v\n", ce)
-		h, changed = out, true
+func parsePercentage(s string) (int, bool) {
+	i, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		return int(i), false
 	}
-	return h, changed
+	return 0, true
 }
 
-func valueString(in uint8) string {
-	s := strconv.Itoa(int(in))
-	delta := 3 - len(s)
-	if delta > 0 {
-		s = strings.Repeat(" ", delta) + s
+func bytesEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
 	}
-	return s
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
 }
