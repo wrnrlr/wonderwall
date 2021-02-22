8d2a6deb6a576c22c8a77a45d010bc1e8f7004db
Werner Laurensse <werner@alman.ax>
2021-02-22 06:31:14 +0100

    colorpicker: Refactor color editor toggling into a separate component

--- colorpicker/colorpicker.go
+++ colorpicker/colorpicker.go
@@ -16,20 +16,19 @@ import (
 	"github.com/wrnrlr/wonderwall/wonder/f32color"
 	"image"
 	"image/color"
-	"math"
 )
 
 func NewColorPicker(th *material.Theme) *ColorPicker {
 	col := color.NRGBA{R: 255, A: 255}
 	cp := &ColorPicker{
-		tone:      &Position{},
-		hue:       &widget.Float{Axis: layout.Horizontal},
-		alfa:      &AlphaSlider{slider: widget.Float{Axis: layout.Horizontal}, color: col},
-		toggle:    &widget.Clickable{},
-		hexEditor: &HexEditor{theme: th, hex: newHexField(widget.Editor{SingleLine: true}, hex.EncodeToString([]byte{col.R, col.G, col.B}))},
-		rgbEditor: &RgbEditor{theme: th, r: &byteField{Editor: widget.Editor{SingleLine: true}}, g: &byteField{Editor: widget.Editor{SingleLine: true}}, b: &byteField{Editor: widget.Editor{SingleLine: true}}},
-		hsvEditor: &HsvEditor{theme: th, h: &degreeField{Editor: widget.Editor{SingleLine: true}}, s: &percentageField{Editor: widget.Editor{SingleLine: true}}, v: &percentageField{Editor: widget.Editor{SingleLine: true}}},
-		theme:     th}
+		tone: &Position{},
+		hue:  &widget.Float{Axis: layout.Horizontal},
+		alfa: &AlphaSlider{slider: widget.Float{Axis: layout.Horizontal}, color: col},
+		editor: NewToggle(&widget.Clickable{},
+			&HexEditor{theme: th, hex: newHexField(widget.Editor{SingleLine: true}, hex.EncodeToString([]byte{col.R, col.G, col.B}))},
+			&RgbEditor{theme: th, r: &byteField{Editor: widget.Editor{SingleLine: true}}, g: &byteField{Editor: widget.Editor{SingleLine: true}}, b: &byteField{Editor: widget.Editor{SingleLine: true}}},
+			&HsvEditor{theme: th, h: &degreeField{Editor: widget.Editor{SingleLine: true}}, s: &percentageField{Editor: widget.Editor{SingleLine: true}}, v: &percentageField{Editor: widget.Editor{SingleLine: true}}}),
+		theme: th}
 	cp.SetColor(col)
 	return cp
 }
@@ -40,15 +39,11 @@ type ColorPicker struct {
 	hue  *widget.Float
 	alfa *AlphaSlider
 
-	hexEditor *HexEditor
-	rgbEditor *RgbEditor
-	hsvEditor *HsvEditor
+	editor *Toggle
 
-	inputType int
-	toggle    *widget.Clickable
-	hsv       HSVColor
-	color     color.NRGBA
-	theme     *material.Theme
+	hsv   HSVColor
+	color color.NRGBA
+	theme *material.Theme
 }
 
 func (cp *ColorPicker) Layout(gtx layout.Context) layout.Dimensions {
@@ -141,34 +136,12 @@ func (cp *ColorPicker) layoutRainbow(gtx layout.Context) layout.Dimensions {
 }
 
 func (cp *ColorPicker) layoutTextInput(gtx layout.Context) layout.Dimensions {
-	macro := op.Record(gtx.Ops)
-	var w layout.Widget
-	switch cp.inputType {
-	case 0:
-		w = cp.hexEditor.Layout
-	case 1:
-		w = cp.rgbEditor.Layout
-	default:
-		w = cp.hsvEditor.Layout
-	}
-	dims := layout.Flex{Axis: layout.Horizontal, Alignment: layout.Middle}.Layout(gtx,
-		layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-			return material.Clickable(gtx, cp.toggle, func(gtx layout.Context) layout.Dimensions {
-				return toggleIcon.Layout(gtx, unit.Dp(30))
-			})
-		}),
-		layout.Flexed(1, w))
-	call := macro.Stop()
-	paint.FillShape(gtx.Ops, color.NRGBA{R: 0xff, G: 0xff, B: 0xff, A: 0xff}, clip.Rect{Max: dims.Size}.Op())
-	call.Add(gtx.Ops)
-	return dims
+	return cp.editor.Layout(gtx)
 }
 
 func (cp *ColorPicker) SetColor(col color.NRGBA) {
 	cp.setColor(col)
-	cp.hexEditor.SetColor(col)
-	cp.rgbEditor.SetColor(col)
-	cp.hsvEditor.SetColor(col)
+	cp.editor.SetColor(col)
 }
 
 func (cp *ColorPicker) setColor(rgb color.NRGBA) {
@@ -192,47 +165,22 @@ func (cp *ColorPicker) Changed() bool {
 		cp.hsv.S = cp.tone.X
 		cp.hsv.V = 1 - cp.tone.Y
 		col := cp.Color()
-		cp.hexEditor.SetColor(col)
-		//cp.rgbEditor.SetColor(col)
-		//cp.hsvEditor.SetColor(col)
+		cp.editor.SetColor(col)
 	}
 	if cp.hue.Changed() {
 		changed = true
 		cp.hsv.H = cp.hue.Value * 360
 		col := cp.Color()
-		cp.hexEditor.SetColor(col)
-		//cp.rgbEditor.SetColor(col)
-		//cp.hsvEditor.SetColor(col)
+		cp.editor.SetColor(col)
 	}
 	if cp.alfa.Changed() {
 		col := cp.Color()
-		cp.hexEditor.SetColor(col)
-		//cp.rgbEditor.SetColor(col)
-		//cp.hsvEditor.SetColor(col)
+		cp.editor.SetColor(col)
 	}
-	if cp.inputType == 0 && cp.hexEditor.Changed() {
+	if cp.editor.Changed() {
 		changed = true
-		col := cp.hexEditor.Color()
+		col := cp.editor.Color()
 		cp.setColor(col)
-		//cp.rgbEditor.SetColor(col)
-		//cp.hsvEditor.SetColor(col)
-	}
-	//if cp.inputType == 1 && cp.rgbEditor.Changed() {
-	//	changed = true
-	//	col := cp.hexEditor.Color()
-	//	cp.setColor(col)
-	//	cp.hexEditor.SetColor(col)
-	//	cp.hsvEditor.SetColor(col)
-	//}
-	//if cp.inputType == 2 && cp.hsvEditor.Changed() {
-	//	changed = true
-	//	col := cp.hexEditor.Color()
-	//	cp.setColor(col)
-	//	cp.hexEditor.SetColor(col)
-	//	cp.rgbEditor.SetColor(col)
-	//}
-	for range cp.toggle.Clicks() {
-		cp.inputType = int(math.Mod(float64(cp.inputType+1), 3))
 	}
 	return changed
 }
