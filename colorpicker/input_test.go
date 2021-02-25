0cdc80689d7e8498a4e7102bba4de47cfb6a0355
Werner Laurensse <werner@alman.ax>
2021-02-25 22:16:16 +0100

    colorpicker: Add mux test

--- colorpicker/input_test.go
+++ colorpicker/input_test.go
@@ -1,12 +1,13 @@
 package colorpicker
 
 import (
+	"encoding/hex"
 	"image/color"
 	"testing"
 )
 
 func TestInputSetColor(t *testing.T) {
-	red := color.NRGBA{R: 0xff, A: 0xff}
+	col := color.NRGBA{R: 0, G: 255, B: 0, A: 255}
 	for _, in := range []ColorInput{
 		NewPicker(nil),
 		NewAlphaSlider(),
@@ -20,12 +21,33 @@ func TestInputSetColor(t *testing.T) {
 		if in.Changed() {
 			t.Fail()
 		}
-		in.SetColor(red)
+		in.SetColor(col)
 		if in.Changed() {
 			t.Fail()
 		}
-		if in.Color() != red {
+		if in.Color() != col {
 			t.Fail()
 		}
+	}
+}
+
+func TestMux(t *testing.T) {
+	col := color.NRGBA{R: 134, G: 99, B: 12, A: 0}
+	hexe := NewHexEditor(nil)
+	rgb := NewRgbEditor(nil)
+	alpha := NewAlphaSlider()
+	mux := NewMux(hexe, rgb, alpha)
+	hexe.hex.editor.SetText(hex.EncodeToString([]byte{col.R, col.G, col.B}))
+	if !mux.Changed() {
+		t.Fail()
+	}
+	if mux.Color() != col {
+		t.Fail()
+	}
+	if rgb.Color() != col {
+		t.Fail()
+	}
+	if alpha.Color() != col {
+		t.Fail()
 	}
 }
